20c737d8720c88a3aa236809d291d24bba4eeecf
MrPatrickChaves <patrickchavescode@gmail.com>
2024-04-05 17:53:09 -0300

    Adicionando env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { EnvConfigModule } from './shared/infraestructure/env-config/env-config.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, EnvConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
